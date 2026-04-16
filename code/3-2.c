20d6979db28a6c62c81c11595a597c71e91a7de7
LuminApo-k <beomseomoon@gmail.com>
2026-04-17 05:58:11 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -5,9 +5,8 @@ int main() {
 
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 3; j++) {
-            printf("%p ", (void*)&arr[i][j]);
+            printf("%p\n", (void*)&arr[i][j]);
         }
-        printf("\n");
     }
 
     return 0;
