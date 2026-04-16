e3f4b9193f91a0edb60f194b52a65dc7216e328e
LuminApo-k <beomseomoon@gmail.com>
2026-04-17 05:57:52 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 void print_arr(int *p, int size) {
+    printf("%lu\n", sizeof(p));
     for (int i = 0; i < size; i++) {
         printf("%d ", *(p + i));
     }
-    printf("\n");
 }
 
 int main() {
-    int arr[5] = {1,2,3,4,5};
+    int arr[5] = {1, 2, 3, 4, 5};
 
+    printf("%lu\n", sizeof(arr));
     for (int i = 0; i < 5; i++) {
         printf("%d ", *(arr + i));
     }
